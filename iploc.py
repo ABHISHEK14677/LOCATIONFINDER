5ab7010f8bed4e215668e1113cc2ee38fb436c5b
ABHISHEK M <m.abhishek14677@gmail.com>
2026-09-13 13:03:24 +0530

    Update iploc.py

--- iploc.py
+++ iploc.py
@@ -325,7 +325,7 @@ def process_ips(ips, mmdb=None, quiet=False):
 
 def interactive():
     
-print(f"{RED}
+print(f"\033[31m
  ██████   ███    ███  ███    ██  ██            █████  ██████   ██   ██  ██
 ██    ██  ████  ████  ████   ██  ██           ██   ██ ██   ██  ██   ██  ██
 ██    ██  ██ ████ ██  ██ ██  ██  ██    ████   ███████ ██████   ███████  ██
@@ -333,7 +333,7 @@ print(f"{RED}
  ██████   ██      ██  ██   ████  ██           ██   ██ ██████   ██   ██  ██
                                                 ~ O M N I - A B H I
   CREATED BY : https://github.com/ABHISHEK14677
-{RESET}")
+\033[0m"")
     print("=" * 60)
     print("IP Location / OSINT Recon".center(60))
     print("=" * 60)
